bbde08f54fca7ff666619989fd297b09ed93baf7
rivermont <william.11bennett@gmail.com>
2017-09-04 18:31:53 -0400

    Added !flip and !roll

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
-import asyncio
-# import random
+# import asyncio
+import random
 # import os
 
 client = discord.Client()
@@ -13,7 +13,28 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.content.startswith('!test'):
-		await client.send_message(message.channel, 'Hello World!')
+	content = message.content
+	if message.content.startswith('!flip'):
+		# Flips a coin on two choices. Defaults to Heads or Tails.
+		if len(content.split()) == 1:
+			choice_ = random.choice(['Heads', 'Tails'])
+			await client.send_message(message.channel, choice_)
+		elif len(content.split()) == 2:
+			await client.send_message(message.channel, 'Only one option supplied. Must be two or none.')
+		elif len(content.split()) == 3:
+			options = content.split()[1:]
+			flip = random.choice(options)
+			await client.send_message(message.channel, flip)
+	elif content.startswith('!roll'):
+		# Rolls a dice. Defaults to a d6.
+		if len(content.split()) == 1:
+			roll = random.randint(1, 6)
+			await client.send_message(message.channel, 'You rolled a {0}.'.format(roll))
+		if len(content.split()) == 2:
+			input_ = content.split()[1]
+			roll = random.randint(1, int(input_))
+			await client.send_message(message.channel, 'You rolled a {0}.'.format(roll))
 
-client.run('Mjg2NTgzNjI3NzkyNTE1MDgz.C5i1bw.Wyh56h4hWkeg8kdS1p0sC73to7A')
+if __name__ == '__main__':
+	print('Logging in...')
+	client.run('Mjg2NTgzNjI3NzkyNTE1MDgz.C5i1bw.Wyh56h4hWkeg8kdS1p0sC73to7A')
