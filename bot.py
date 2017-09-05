b5d08677a31a291fb2baebd28c796262a1e29b9e
rivermont <william.11bennett@gmail.com>
2017-09-05 12:11:30 -0400

    Updated bot

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ import markovify
 
 client = discord.Client()
 
+read = []
+
 
 def add_msg(channel, text):
 	"""
@@ -30,10 +32,15 @@ async def on_ready():
 	print('Logged in as {0}; ID #{1}'.format(client.user.name, client.user.id))
 	print('Setting status...')
 	await client.change_presence(game=discord.Game(name='https://github.com/rivermont/orka'))
-	# for server in client.servers:
-	# 	for channel in server.channels:
-	# 		if channel.type == 'text':
-	# 			print(channel.permissions_for(client))
+	for server in client.servers:
+		for channel in server.channels:
+			if channel.type == discord.ChannelType.text:
+				if channel.permissions_for(server.me).read_messages:
+					print('Read access in: ' + server.name + '/' + channel.name)
+					read.append(channel)
+	for channel in read:
+		async for m in client.logs_from(channel):
+			add_msg(channel, m.content)
 
 
 @client.event
@@ -77,17 +84,14 @@ async def on_message(message):
 			if '(status code: 400)' in str(e):
 				print('Failed to create sentence.')
 
-	# elif content.startswith('@logs'):
-	# 	async for m in client.logs_from(channel):
-	# 		add_msg(channel, m.content)
+	elif content.startswith('@logs'):
+		async for m in client.logs_from(channel):
+			add_msg(channel, m.content)
 
 	elif content.startswith('@generate'):
 		print('Generating markov model for channel {0}'.format(channel))
 		make_markov_model(channel)
 		await client.send_message(channel, 'Successfully generated markov model.')
-
-	elif content.startswith('$4'):
-		await client.send_message(channel, '!sentence')
 
 
 if __name__ == '__main__':
